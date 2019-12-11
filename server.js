63a9f8def94e61520a5fb466fff0b8aad97659f0
cindyweiss <weiss_cindy@yahoo.com>
2019-12-11 10:56:04 -0800

    saving work mid day

--- server.js
+++ server.js
@@ -5,7 +5,20 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//routes
+app.get('/location', (request, response))
+console.log('I am listening on the location route')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// "search_query": "seattle",
+// "formatted_query": "Seattle, WA, USA",
+// "latitude": "47.606210",
+// "longitude": "-122.332071"
+// }
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('*', (request, response) => {
+  response.status(404).send('page not found');
+})
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
